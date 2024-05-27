f8d70beaf101df56b1ac852a1e8a03be14252e41
matii03 <matiasmonetti19@gmail.com>
2024-05-27 19:33:13 -0300

    plataforma movible

---
+++ Assets/03_Scripts/Plataform.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataform : MonoBehaviour
+{
+    [SerializeField] Transform[] _waypoints;
+    [SerializeField] float _maxVelocity;
+    [SerializeField] float _maxForce;
+    private int _actualIndex;
+    private Vector3 _velocity;
+
+    private void Update()
+    {
+        AddForce(Seek(_waypoints[_actualIndex].position));
+        if(Vector3.Distance(transform.position, _waypoints[_actualIndex].position)<=0.3f)
+        {
+            _actualIndex++;
+            if (_actualIndex >= _waypoints.Length)
+                _actualIndex = 0;
+        }
+        transform.position += _velocity * Time.deltaTime;
+        transform.forward = _velocity;
+    }
+
+    Vector3 Seek(Vector3 target)
+    {
+        var desired = target - transform.position;
+        desired.Normalize();
+        desired *= _maxVelocity;
+        return desired;
+    }
+
+    void AddForce(Vector3 dir)
+    {
+        _velocity += dir;
+
+        _velocity = Vector3.ClampMagnitude(_velocity, _maxVelocity);
+    }
+}
