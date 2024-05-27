becc0655a96516fef0fb8aba8fee1df46259152c
matii03 <matiasmonetti19@gmail.com>
2024-05-27 18:20:19 -0300

    animaciones con golpe

--- Assets/03_Scripts/Players/Monkey/NormalPunch.cs
+++ Assets/03_Scripts/Players/Monkey/NormalPunch.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class NormalPunch : Hits
 {
+    [SerializeField] float _damage;
+
     private void OnTriggerEnter(Collider other)
     {
         var target = other.gameObject.GetComponent<IDamageable>();
 
         if (target != null)
         {
-            target.TakeDamageEntity(_damagePunch, _entity.transform.position);
+            target.TakeDamageEntity(_damage, _entity.transform.position);
         }
     }
 }
