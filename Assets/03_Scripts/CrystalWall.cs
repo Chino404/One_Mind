ba32aecc3fe094c1a5894d022bf903ffc8a5d27a
matii03 <matiasmonetti19@gmail.com>
2024-06-03 17:23:13 -0300

    tutoriales y pared de cristal programado

---
+++ Assets/03_Scripts/CrystalWall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalWall : MonoBehaviour
+{
+    [SerializeField] GameObject _crystalWall;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Enemy>())
+            _crystalWall.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (GameManager.instance.enemies.Count < 1)
+            _crystalWall.SetActive(false);
+
+    }
+}
