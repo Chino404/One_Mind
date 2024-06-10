bb3314b22957b822a4f3b57bd0afe26d17809f46
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-06-09 23:26:57 -0300

    Comportamiento de banana

---
+++ Assets/03_Scripts/Players/Banana/WayPoints.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    [SerializeField] private bool _stop;
+    public bool Stop { get { return _stop; } }
+
+    public bool enemies;
+}
