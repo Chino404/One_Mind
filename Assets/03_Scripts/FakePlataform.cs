4b5658b5daeab067abf2b5c4eb911d88ce87eb39
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-09-23 12:21:38 -0300

    Shader Disolve en plataformas

--- Assets/03_Scripts/FakePlataform.cs
+++ Assets/03_Scripts/FakePlataform.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class FakePlataform : Rewind
 {
-    
+    [SerializeField, Range(0.1f, 1)] private float _timeToDisolve = 0.25f;
+    private Renderer _disolveMaterial;
+    private int _IdDisolve = Shader.PropertyToID("_Disolve");
+
+    private void Start()
+    {
+        _disolveMaterial = GetComponent<Renderer>();
+    }
 
     public override void Save()
     {
@@ -21,11 +28,29 @@ public class FakePlataform : Rewind
 
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Characters>())
         {
-            gameObject.SetActive(false);
+            StartCoroutine(ShaderDisolve());
         }
+    }
+
+    IEnumerator ShaderDisolve()
+    {
+        var timer = 0f;
+        var valueDisolve = 0f;
+
+        while (timer < _timeToDisolve)
+        {
+            timer += Time.deltaTime;
+            float t = timer / _timeToDisolve;
+            valueDisolve = Mathf.Lerp(1, 0, t);
+
+            _disolveMaterial.material.SetFloat(_IdDisolve, valueDisolve);
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
     }
 }
