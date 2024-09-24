7e393a0712be7db8ec1f7d9ada837db87699bc37
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-09-24 16:15:15 -0300

    cambios clases 24/09

--- Assets/03_Scripts/FakePlataform.cs
+++ Assets/03_Scripts/FakePlataform.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class FakePlataform : Rewind
 {
     [SerializeField, Range(0.1f, 1)] private float _timeToDisolve = 0.25f;
+    private float _valueDisolve = 1f;
     private Renderer _disolveMaterial;
     private int _IdDisolve = Shader.PropertyToID("_Disolve");
 
     private void Start()
     {
         _disolveMaterial = GetComponent<Renderer>();
+        _valueDisolve = 1f;
     }
 
     public override void Save()
     {
-        _currentState.Rec(gameObject.activeInHierarchy);
+        _currentState.Rec(gameObject.activeInHierarchy, _valueDisolve);
     }
 
     public override void Load()
@@ -25,7 +27,13 @@ public class FakePlataform : Rewind
         var col = _currentState.Remember();
 
         gameObject.SetActive((bool)col.parameters[0]);
+        _valueDisolve = (float)col.parameters[1];
 
+    }
+
+    private void Update()
+    {
+        _disolveMaterial.material.SetFloat(_IdDisolve, _valueDisolve);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,15 +47,15 @@ public class FakePlataform : Rewind
     IEnumerator ShaderDisolve()
     {
         var timer = 0f;
-        var valueDisolve = 0f;
+        //var valueDisolve = 0f;
 
         while (timer < _timeToDisolve)
         {
             timer += Time.deltaTime;
             float t = timer / _timeToDisolve;
-            valueDisolve = Mathf.Lerp(1, 0, t);
+            _valueDisolve = Mathf.Lerp(1, 0, t);
 
-            _disolveMaterial.material.SetFloat(_IdDisolve, valueDisolve);
+            _disolveMaterial.material.SetFloat(_IdDisolve, _valueDisolve);
             yield return null;
         }
 
