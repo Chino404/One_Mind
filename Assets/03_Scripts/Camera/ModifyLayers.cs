20bbe2823cca77f07dfaaaa1912711e18051974c
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-10-03 14:26:29 -0300

    Codigo para cambiar las layers

--- Assets/03_Scripts/Camera/ModifyLayers.cs
+++ Assets/03_Scripts/Camera/ModifyLayers.cs
@@ -6,26 +6,32 @@ using UnityEngine.UI;
 public class ModifyLayers : MonoBehaviour, IInteracteable
 {
     [SerializeField, Tooltip("Capa en la que no se renderiza los objetos para la cámara | Poner solo una Layer")] private LayerMask _doNotRenderer;
-    private int _valueMask;
+    private int _valueMaskNotRenderer;
 
-    [SerializeField, Tooltip("Objetos que se van a ACTIVAR para la cámara")] private GameObject[] _activeLayers;
+    [Space(10), SerializeField, Tooltip("Objetos que se van a ACTIVAR para la cámara")] private GameObject[] _activeLayers;
     private Queue<int> _listSaveActiveLayers;
+    [SerializeField, Tooltip("Volver a desactivar las layer que se activaron cuando salga del Collider")] private bool _backToDeactive;
 
-    [SerializeField, Tooltip("Objetos que se van a DESACTIVAR para la cámara")] private GameObject[] _desactiveLayers;
+    [Space(10), SerializeField, Tooltip("Objetos que se van a DESACTIVAR para la cámara")] private GameObject[] _desactiveLayers;
     private Queue<int> _listSaveDesactiveLayers;
+
+    private Collider _myCollider;
 
     private void Awake()
     {
+        if (gameObject.GetComponent<Collider>()) _myCollider = GetComponent<Collider>();
+        else Debug.LogError($"Falta Collider en: {gameObject.name}");
+
         _listSaveActiveLayers = new Queue<int>();
         _listSaveDesactiveLayers = new Queue<int>();
 
-        _valueMask = (int)Mathf.Log(_doNotRenderer.value, 2);
+        _valueMaskNotRenderer = (int)Mathf.Log(_doNotRenderer.value, 2);
 
         for (int i = 0; i < _activeLayers.Length; i++)
         {
-            _listSaveActiveLayers.Enqueue(_activeLayers[i].layer);
+            _listSaveActiveLayers.Enqueue(_activeLayers[i].gameObject.layer);
 
-            _activeLayers[i].layer = _doNotRenderer;
+            _activeLayers[i].layer = _valueMaskNotRenderer;
         }
 
         for (int i = 0; i < _desactiveLayers.Length; i++)
@@ -33,24 +39,39 @@ public class ModifyLayers : MonoBehaviour, IInteracteable
             _listSaveDesactiveLayers.Enqueue(_desactiveLayers[i].gameObject.layer);
         }
 
+        //foreach (var item in _listSaveActiveLayers)
+        //{
+        //    Debug.Log($"En {gameObject.name}: {item}");
+        //}
+
     }
 
     public void Active()
-    {
+    {                                                                                                                                                        
         for (int i = 0; i < _activeLayers.Length; i++)
         {
-            _activeLayers[i].layer = _listSaveActiveLayers.Peek();
+            _activeLayers[i].layer = _listSaveActiveLayers.Dequeue();
         }
 
         for (int i = 0; i < _desactiveLayers.Length; i++)
         {
-            _desactiveLayers[i].layer = _valueMask;
+            _desactiveLayers[i].layer = _valueMaskNotRenderer;
         }
+
+        if(!_backToDeactive) _myCollider.enabled = false;
     }
 
     public void Deactive()
     {
-        
+        if(_backToDeactive)
+        {
+            for (int i = 0; i < _activeLayers.Length; i++)
+            {
+                _listSaveActiveLayers.Enqueue(_activeLayers[i].layer);
+
+                _activeLayers[i].layer = _valueMaskNotRenderer;
+            }
+        }
     }
 
 }
