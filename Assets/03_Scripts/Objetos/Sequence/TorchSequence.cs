5ee8d9aae1c6cca358245a134be0bfed47f811bc
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-10-03 19:52:59 -0300

    Cambio de Color al iniciar secuencia | Antorchas

--- Assets/03_Scripts/Objetos/Sequence/TorchSequence.cs
+++ Assets/03_Scripts/Objetos/Sequence/TorchSequence.cs
@@ -7,7 +7,7 @@ public class TorchSequence : MonoBehaviour
     [SerializeField,Tooltip("Las anotrchas que van en la secuencia")] private ActiveTorch[] _torchs;
 
     [SerializeField, Tooltip("Color del fuego para cuando arranque la secuencia")] private Color _ChangeColorFire;
-    private Color _saveColorFire;
+    [SerializeField] private List<Color> _saveColorFire;
 
     [Space(10),SerializeField,Tooltip("El indice de la antorcha a prenderse")] public List<int> sequenceList;
     [Space(10),Range(0, 1.5f), Tooltip("Tiempo que las antorchas estan activadas antes de pasar a la otra | VARIABLE PÚBLICA")] public float activeTime;
@@ -16,7 +16,8 @@ public class TorchSequence : MonoBehaviour
     {
         for (int i = 0; i < _torchs.Length; i++)
         {
-            if (_torchs[i] == null)Debug.LogError($"Hay un indice vacio en {gameObject.name}");
+            if (_torchs[i] == null) Debug.LogError($"Hay un indice vacio en {gameObject.name}");
+            else _saveColorFire.Add(_torchs[i].ColorFire);
         }
     }
 
@@ -80,8 +81,8 @@ public class TorchSequence : MonoBehaviour
 
             for (int i = 0; i < _torchs.Length; i++)
             {
-                _torchs[i].ChangeColorFire(_saveColorFire);
                 _torchs[i].Deactive();
+                _torchs[i].ChangeColorFire(_saveColorFire[i]);
             }
 
             yield return new WaitForSeconds(0.1f);
