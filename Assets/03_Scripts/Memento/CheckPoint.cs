365f2bd69d6048bb9ea2d3e0b71cfbff758483d3
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-10-08 17:49:09 -0300

    Cambios de checkpoints lvl 2

--- Assets/03_Scripts/Memento/CheckPoint.cs
+++ Assets/03_Scripts/Memento/CheckPoint.cs
@@ -4,27 +4,61 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    [HideInInspector]public Transform spawnPoint;
+    [SerializeField, Tooltip("La referencia del Checkpoint al que va a estar vinculado")] private CheckPoint _refOtherCheckPoint;
+
+    private Transform _spawnPoint;
+    public Transform SpawnPoint { get { return _spawnPoint; } }
+
+    private Characters _playerRef;
+
+    private bool _inPosition;
+    public bool InPosition { get { return _inPosition; } }
     
     private void Awake()
     {
-        
-        spawnPoint = transform.GetChild(0);
-    }
-    
+        _spawnPoint = transform.GetChild(0);
 
+        if (!_refOtherCheckPoint) Debug.LogError($"Falra la referencia del otro checkpoint en: {gameObject.name}");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<Characters>())
         {
-            other.GetComponent<Characters>().actualCheckpoint = this;
+            _playerRef = other.GetComponent<Characters>();
 
-            foreach (var item in GameManager.instance.rewinds)
+            _inPosition = true;
+
+            if(_refOtherCheckPoint.InPosition)
             {
-                item.Save();
+                SaveCheckpoint();
+                _refOtherCheckPoint.SaveCheckpoint();
             }
-        }
 
+            //other.GetComponent<Characters>().actualCheckpoint = this;
+
+            //foreach (var item in GameManager.instance.rewinds)
+            //{
+            //    item.Save();
+            //}
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.GetComponent<Characters>())
+        {
+            _inPosition = false;
+        }
+    }
+
+    public void SaveCheckpoint()
+    {
+        _playerRef.actualCheckpoint = this;
+
+        foreach (var item in GameManager.instance.rewinds)
+        {
+            item.Save();
+        }
     }
 }
