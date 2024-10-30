3f73e92283c7a26f1dfcaff957513e936535ffe2
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-10-30 13:11:56 -0300

    Mini cambios en lvl 3, comienzo de programación transparencia

--- Assets/03_Scripts/Objetos/DualDoor.cs
+++ Assets/03_Scripts/Objetos/DualDoor.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DualDoor : MonoBehaviour
+public class DualDoor : MonoBehaviour, ITransparency
 {
     private Animator _animator;
 
+    [SerializeField, Tooltip("El marco de la puerta")] private Renderer _objectTransparency;
+    private int _idOpacity = Shader.PropertyToID("_Opacity");
+    private float _actualValueAlpha;
+
     private void Awake()
-    {
-        
+    {    
         _animator = GetComponent<Animator>();
+        _actualValueAlpha = 1;
     }
 
     public void OpenTheDoor()
@@ -20,5 +24,37 @@ public class DualDoor : MonoBehaviour
 
     }
 
-    
+    public void Fade(float t)
+    {
+        
+    }
+
+    IEnumerator InterpolateFade(float durationFade)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < durationFade)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / durationFade;
+
+            // Interpolar el valor alfa
+            //float alpha = Mathf.Lerp(_iniColor.a, _valueAlpha, t);
+            float alpha = Mathf.Lerp(1, 0, t);
+
+            // Crear un nuevo color con el alfa interpolado, manteniendo los valores RGB originales
+            //_material.color = new Color(_iniColor.r, _iniColor.g, _iniColor.b, alpha);
+
+
+            yield return null;
+        }
+
+        // Asegúrate de que el color final sea el correcto
+        //_material.color = new Color(_iniColor.r, _iniColor.g, _iniColor.b, _valueAlpha);
+    }
+
+    public void Appear(float t)
+    {
+        
+    }
 }
