570d8aa5b2daa21f09a61f1eb541ae47b8cae0fb
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-11-06 23:37:12 -0300

    casi fin del TargetLocked

--- Assets/03_Scripts/Camera/FixativeObjectUI.cs
+++ Assets/03_Scripts/Camera/FixativeObjectUI.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
+using static UnityEngine.GraphicsBuffer;
 
 public class FixativeObjectUI : MonoBehaviour
 {
@@ -8,34 +10,108 @@ public class FixativeObjectUI : MonoBehaviour
     //especialmente en el sistema de Canvas. Es una extensión de Transform y está específicamente diseñado para trabajar con interfaces y elementos en 2D.
 
     private Camera _refCamera;  // La cámara principal que renderiza la escena
+    private ModelBongo _refModelBongo;
     private Vector3 _screenPosition;
+
     public RectTransform canvasRectTransform;  // El RectTransform del canvas
     public RectTransform targetLockImage;  // El RectTransform de la imagen en el canvas que indica el target lock
 
-    private Transform _targetObject;  // El objeto en la escena al que queremos hacer target lock
+    [Space(10),SerializeField]private List<Transform> _listTargetObject;  // El objeto en la escena al que queremos hacer target lock
+    [SerializeField] private int _currentIndexList; //Se muestra para chequear
+    private Vector3 _direction;
+
+    private float _scroll; //Temporal
+    private bool _isTargetDetected;
+
     private Collider _myBoxCollider;
 
     private void Awake()
     {
         _myBoxCollider = GetComponent<BoxCollider>();
+        _refModelBongo = GetComponent<ModelBongo>();
     }
 
     private void Start()
     {
         _refCamera = GameManager.instance.bongoCamera.GetComponent<Camera>();
+        _currentIndexList = 0;
     }
 
     void Update()
     {
-        
+        // Obtiene el valor de desplazamiento de la rueda del ratón
+        //_scroll = Input.GetAxisRaw("Mouse ScrollWheel");
 
-        
+        //if(_scroll > 0f)
+        //{
+        //    _currentIndexList--;
+
+        //    if (_currentIndexList < 0) _currentIndexList = _listTargetObject.Count - 1;
+        //}
+
+        //else if (_scroll < 0f)
+        //{
+        //    _currentIndexList++;
+
+        //    if (_currentIndexList >= _listTargetObject.Count) _currentIndexList = 0;
+        //}
+
+        //if (_listTargetObject.Count > 0) TargetLockA();
+        //else _refModelBongo.DirSpells = Vector3.zero;
+
     }
 
-    private void TargetLokc()
+    private bool ObstructionRay(Vector3 obj)
     {
+        _direction = obj - transform.position;
+
+        if (Physics.Raycast(transform.position, _direction, out RaycastHit hit, _direction.magnitude))
+        {
+            if (hit.transform.gameObject.layer != 10) _isTargetDetected = true;
+
+            else _isTargetDetected = false;
+        }
+
+        return _isTargetDetected;
+    }
+
+    public void TargetLockA(float scroll)
+    {
+        if(_listTargetObject.Count <= 0)
+        {
+            _refModelBongo.DirSpells = Vector3.zero;
+            return;
+        }
+
+
+        if (scroll > 0f)
+        {
+            var aux = _currentIndexList; //Variable auxiliar
+            _currentIndexList--;
+
+            if (_currentIndexList < 0) _currentIndexList = _listTargetObject.Count - 1;
+
+            if (ObstructionRay(_listTargetObject[_currentIndexList].position)) _currentIndexList = aux;
+        }
+
+        else if (scroll < 0f)
+        {
+            var aux = _currentIndexList; //Variable auxiliar
+            _currentIndexList++;
+
+            if (_currentIndexList >= _listTargetObject.Count) _currentIndexList = 0;
+
+            if (ObstructionRay(_listTargetObject[_currentIndexList].position)) _currentIndexList = aux;
+        }
+
+        //if (ObstructionRay(_listTargetObject[_currentIndexList].position)) LogicaDelMasCecano();
+
         // Verifica que el objeto esté dentro de la vista de la cámara
-        _screenPosition = _refCamera.WorldToScreenPoint(_targetObject.position);
+        _screenPosition = _refCamera.WorldToScreenPoint(_listTargetObject[_currentIndexList].position);
+
+        _direction = _listTargetObject[_currentIndexList].position - transform.position;
+
+        Debug.DrawLine(transform.position, _listTargetObject[_currentIndexList].position, Color.cyan);
 
         if (_screenPosition.z > 0)  // Asegura que el objeto está en frente de la cámara
         {
@@ -47,19 +123,54 @@ public class FixativeObjectUI : MonoBehaviour
 
             // Asegura que la imagen esté visible
             targetLockImage.gameObject.SetActive(true);
+
+            _refModelBongo.DirSpells = _direction;
         }
         else
         {
             // Si el objeto está detrás de la cámara, oculta la imagen
+            Debug.Log("Es menor a 0");
+            _refModelBongo.DirSpells = Vector3.zero;
             targetLockImage.gameObject.SetActive(false);
         }
+
     }
+
+    private void LogicaDelMasCecano()
+    {
+        var masCercano = 10000f;
+        var newIndex = 0;
+
+        for (int i = 0; i < _listTargetObject.Count; i++)
+        {
+            var newDistance = Vector3.Distance(transform.position, _listTargetObject[i].position);
+
+            if (newDistance < masCercano)
+            {
+                masCercano = newDistance;
+                newIndex = i;
+            }
+        }
+
+        _currentIndexList = newIndex;
+    }
+
+    private void RemoveObjectToList(Transform obj)
+    {
+        _listTargetObject.Remove(obj);
+
+        if (_currentIndexList > 0) _currentIndexList--;
+        else targetLockImage.gameObject.SetActive(false);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 10)
+        var objInteract = other.gameObject;
+
+        if (objInteract.layer == 10)
         {
-             _targetObject = other.gameObject.transform;
+            if(!_listTargetObject.Contains(objInteract.transform)) _listTargetObject.Add(objInteract.transform);
         }
     }
 
@@ -67,7 +178,7 @@ public class FixativeObjectUI : MonoBehaviour
     {
         if (other.gameObject.layer == 10)
         {
-             _targetObject = null;
+            RemoveObjectToList(other.gameObject.transform);
         }
     }
 
--- Assets/03_Scripts/Players/Bongo/ModelBongo.cs
+++ Assets/03_Scripts/Players/Bongo/ModelBongo.cs
@@ -12,6 +12,20 @@ public class ModelBongo : Characters
     private ControllerBongo _controller;
     private ViewBongo _view;
 
+    [Header("--- BONGO'S VALUES ---")]
+    [SerializeField, Tooltip("Prefab del Spell")] private Bullet _spellPrefab;
+    private Vector3 _dirSpells;
+    public Vector3 DirSpells { set { _dirSpells = value; } }
+    [SerializeField, Tooltip("Cantidad de balas que instancio al principio")] int _bulletQuantity;
+
+    [SerializeField, Tooltip("Segundos de cooldown")] float _seconds;
+    [HideInInspector] public float valueScroll;
+    bool _coolDown;
+
+    private FixativeObjectUI _targetLock;
+    private Factory<Bullet> _factory;
+    private ObjectPool<Bullet> _objectPool;
+
 
     public override void Awake()
     {
@@ -29,11 +43,17 @@ public class ModelBongo : Characters
         _controller = new ControllerBongo(this, _view);
 
         _currentState = new MementoState();
+        _targetLock = GetComponent<FixativeObjectUI>();
     }
 
     public override void Start()
     {
         base.Start();
+
+        _factory = new BulletFactory(_spellPrefab);
+        _objectPool = new ObjectPool<Bullet>(_factory.GetObj, Bullet.TurnOff, Bullet.TurnOn, _bulletQuantity);
+
+
         ActualMove = NormalMovement;
     }
 
@@ -42,6 +62,8 @@ public class ModelBongo : Characters
         base.Update();
 
         _controller.ArtificialUpdate();
+
+        _targetLock.TargetLockA(valueScroll);
     }
 
     public override void FixedUpdate()
@@ -54,6 +76,23 @@ public class ModelBongo : Characters
     public override void Attack()
     {
         //base.Attack();
+
+        if (!_coolDown) StartCoroutine(Shoot());
+
+    }
+
+    IEnumerator Shoot()
+    {
+        _coolDown = true;
+        var bullet = _objectPool.Get(); //Pido un Spell al _objectPool
+        bullet.AddReference(_objectPool); //Le añado la referencia a mi prefab
+        bullet.transform.position = transform.position; //Le asigno el lugar por donde va a salir
+
+        if(_dirSpells != Vector3.zero) bullet.transform.forward = _dirSpells.normalized;
+        else bullet.transform.forward = transform.forward; //Asigno su dirección
+
+        yield return new WaitForSeconds(_seconds);
+        _coolDown = false;
 
     }
 
