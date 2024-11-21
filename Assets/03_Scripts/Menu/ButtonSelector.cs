26fb027867575b38802f42380e04b186e50dd082
Matias Sepulveda <ezesepulveda444@gmail.com>
2024-11-21 17:18:25 -0300

    Comienzo imagen coleccionables menu

--- Assets/03_Scripts/Menu/ButtonSelector.cs
+++ Assets/03_Scripts/Menu/ButtonSelector.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +7,34 @@ public class ButtonSelector : MonoBehaviour
     private Animator _myAnimator;
     private Button _button;
 
+    public Image[] _imageCollectable;
+
     private void Awake()
     {
         _myAnimator = GetComponent<Animator>();
         _button = GetComponent<Button>();
     }
 
+    public void PointEnterFunc()
+    {
+        if (!_button.interactable) return;
+
+        for (int i = 0; i < _imageCollectable.Length; i++)
+        {
+            _imageCollectable[i].gameObject.SetActive(true);
+        }
+    }
+
     public void PointExitFunc()
     {
-        
-        if(_button.interactable) _myAnimator.SetTrigger("PointExit");
+        if (!_button.interactable) return;
+
+        _myAnimator.SetTrigger("PointExit");
+
+        for (int i = 0; i < _imageCollectable.Length; i++)
+        {
+            _imageCollectable[i].gameObject.SetActive(false);
+        }
+
     }
 }
