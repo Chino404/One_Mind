8e804643bebb48a09d0d649fefe38cbdf37654c6
Matias Sepulveda <ezesepulveda444@gmail.com>
2025-01-14 00:39:02 -0300

    Comienzo ObjectPool de cajas

--- Assets/03_Scripts/ObjectPool/Bullet/BulletFactory.cs
+++ Assets/03_Scripts/ObjectPool/Bullet/BulletFactory.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BulletFactory : Factory<Bullet>
 {
-    public BulletFactory(Bullet p)//en un constructor se entra cuando se crea
+    public BulletFactory(Bullet p)//En un constructor se entra cuando se crea
     {
         prefab = p;
     }
