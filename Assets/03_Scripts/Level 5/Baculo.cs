a26efbbbbf4537307e26e28457a8496360f8f421
matii03 <matiasmonetti19@gmail.com>
2025-02-12 19:00:00 -0300

    golpes para el nivel 5 (falta pulir)

--- Assets/03_Scripts/Level 5/Baculo.cs
+++ Assets/03_Scripts/Level 5/Baculo.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class Baculo : Weapons
 {
-    
+    [SerializeField] private float _activationRange = 5f; 
+    [SerializeField] private LayerMask _obstructionLayers; 
+    public override void Attack()
+    {
+        base.Attack();
+        
+        Debug.Log("ataco con el baculo");
+        Collider[] objectsInRange = Physics.OverlapSphere(transform.position, _activationRange);
+
+        foreach (Collider obj in objectsInRange)
+        {
+            BuildableObject activatable = obj.GetComponent<BuildableObject>();
+
+            if (activatable != null && HasLineOfSight(obj.transform))
+            {
+                activatable.Activate();
+                break; // Activamos solo un objeto
+            }
+        }
+    }
+
+    private bool HasLineOfSight(Transform target)
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (Physics.Raycast(transform.position, direction, distance, _obstructionLayers))
+        {
+            return false; // Hay un obstáculo en el camino
+        }
+
+        return true; // No hay obstrucciones
+    }
 }
+
--- Assets/03_Scripts/Level 5/Peak.cs
+++ Assets/03_Scripts/Level 5/Peak.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Peak : Weapons
 {
+    public GameObject hit;
+    public override void Attack()
+    {
+        
+        base.Attack();
+        Debug.Log("ataco con el pico");
+        StartCoroutine(Hit());
+    }
+
     
+
+    IEnumerator Hit()
+    {
+        _isAttacking = true;
+        hit.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        hit.SetActive(false);
+        _isAttacking = false;
+    }
 }
