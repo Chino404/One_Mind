801a902cabc3a79e812eb9602f331ad058968bae
Chonastradamus <131836132+Chonastradamus@users.noreply.github.com>
2025-03-12 13:02:19 -0300

    level 2 new platforms

--- Assets/03_Scripts/06_Objects/Platforms/FakePlataform.cs
+++ Assets/03_Scripts/06_Objects/Platforms/FakePlataform.cs
@@ -6,32 +6,42 @@ public class FakePlataform : Rewind
 {
     [SerializeField, Range(0.1f, 1), Tooltip("Tiempo de la transición del Disolve")] private float _timeToDisolve = 0.25f;
     [SerializeField, Tooltip("Tiempo que tarda en respawnear cuando se DESACTIVO")] private float _timeToRespawn = 1.5f;
-    [SerializeField]private float _valueDisolve;
-    private Renderer _disolveMaterial;
+    [SerializeField] private float _valueDisolve;
+    [SerializeField] public Renderer[] _disolveRenderers;
+    private List<Material> _disolveMaterials = new List<Material>(); // Lista para almacenar los materiales 
     private int _IdDisolve = Shader.PropertyToID("_Disolve");
 
     public override void Awake()
     {
         base.Awake();
-        _disolveMaterial = GetComponent<Renderer>();
-        _valueDisolve = 0f;     
-    } 
+
+        foreach (var renderer in _disolveRenderers)
+        {
+            _disolveMaterials.AddRange(renderer.materials);
+        }
+        _valueDisolve = 0f;
+    }
 
     private void OnEnable()
     {
         StartCoroutine(ShaderDisolve(false));
     }
 
-    
+
 
     private void Update()
     {
-        _disolveMaterial.material.SetFloat(_IdDisolve, _valueDisolve);
+        foreach (var material in _disolveMaterials)
+        {
+
+            material.SetFloat(_IdDisolve, _valueDisolve);
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Characters>() && _valueDisolve==1)
+        if (other.GetComponent<Characters>() && _valueDisolve == 1)
         {
             StartCoroutine(ShaderDisolve(true));
         }
@@ -41,16 +51,19 @@ public class FakePlataform : Rewind
     {
         var timer = 0f;
 
-        if(desactive)
+        if (desactive)
         {
             while (timer < _timeToDisolve)
             {
                 timer += Time.deltaTime;
                 float t = timer / _timeToDisolve;
                 _valueDisolve = Mathf.Lerp(1, 0, t);
+                foreach (var item in _disolveMaterials)
+                {
 
-                _disolveMaterial.material.SetFloat(_IdDisolve, _valueDisolve);
-            
+                    item.SetFloat(_IdDisolve, _valueDisolve);
+                }
+
                 yield return null;
             }
 
@@ -65,7 +78,11 @@ public class FakePlataform : Rewind
             timer += Time.deltaTime;
             float t = timer / _timeToDisolve;
             _valueDisolve = Mathf.Lerp(0, 1, t);
-            _disolveMaterial.material.SetFloat(_IdDisolve, _valueDisolve);
+            foreach (var item in _disolveMaterials)
+            {
+
+                item.SetFloat(_IdDisolve, _valueDisolve);
+            }
             yield return null;
         }
     }
