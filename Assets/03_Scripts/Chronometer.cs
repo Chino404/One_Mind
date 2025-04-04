d5fa80c81d3381385737974a34048c0bf371b1fe
Matias Sepulveda <ezesepulveda444@gmail.com>
2025-04-03 23:51:30 -0300

    Cronometro Warning.
    Hice una mini animacion para que el cronometro te avise si perdiste o no la estrella. Una para cuando va el 75% del tiempo y otra para cuando ya se paso el tiempo.
    
    Tambien agregue un mejor tiempo en el lvl 2. Ya se puede jugar en modo cronometro.

--- Assets/03_Scripts/Chronometer.cs
+++ Assets/03_Scripts/Chronometer.cs
@@ -5,20 +5,48 @@ using TMPro;
 
 public class Chronometer : MonoBehaviour
 {
+    [Header("-> Reference")]
+    public GameObject secondsInGame;
+
+    private TextMeshProUGUI _txtSecondsInGame;
+
+    [Space(5), Header("-> Change Scale")]
+    [SerializeField, Tooltip("Duración del tiempo de modificacion de la escala") , Range(0,0.5f)]public float _duration = 0.15f; // Duración del efecto
+    [SerializeField, Tooltip("La escala a la que va a transicionar")] private Vector3 _newScale;
+    private Vector3 _originalScale;
+
+    private enum TypeWarning { None, Warning, LostStar}
+    private TypeWarning _myTypeWarning;
+
+    private bool _isChangeWarningScale;
+
+
     private float _time;
-    public TextMeshProUGUI secondsInGame;
+
+    [Space(10), Header("-> Colors Warning")]
+    [SerializeField] private Color _warningColor;
+    [SerializeField] private Color _lostStarColor;
 
 
     private void Awake()
     {
         bool playWithTimer = CallJson.instance.refJasonSave.GetSaveData.playWithTimer;
 
+        _myTypeWarning = TypeWarning.None;
+
         gameObject.SetActive(playWithTimer);
     }
 
     private void Start()
     {
-        secondsInGame.gameObject.SetActive(true);
+        secondsInGame.SetActive(true);
+
+        _txtSecondsInGame = secondsInGame.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        _originalScale = _txtSecondsInGame.transform.localScale;
+
+        //Guardo la referencia en el GameManager
+        GameManager.instance.secondsInGame = secondsInGame;
+
         CallJson.instance.refJasonSave.GetSaveData.playWithTimer = false;
         CallJson.instance.refJasonSave.SaveJSON();
 
@@ -40,8 +68,51 @@ public class Chronometer : MonoBehaviour
         int seconds = Mathf.FloorToInt(_time % 60);
         float fractionalSeconds = _time % 1;
         int decimals = Mathf.FloorToInt(fractionalSeconds * 100);
+
+        if (_time > GameManager.instance.BestTimesInLevel[0].TimeInSeconds && _myTypeWarning == TypeWarning.Warning)
+        {
+            _myTypeWarning = TypeWarning.LostStar;
+            _txtSecondsInGame.color = _lostStarColor;
+
+            StartCoroutine(WarningScale());
+        }
+        else if (_time > GameManager.instance.BestTimesInLevel[0].TimeInSeconds * 0.75f && _myTypeWarning == TypeWarning.None)
+        {
+            _myTypeWarning = TypeWarning.Warning;
+            _txtSecondsInGame.color = _warningColor;
+
+            StartCoroutine(WarningScale());
+        }
         
         //secondsInGame.text = string.Format("{0:00:00}:{1:00:00}", minutes, seconds);
-        secondsInGame.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds,decimals);
+        _txtSecondsInGame.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds,decimals);
+    }
+
+    IEnumerator WarningScale()
+    {
+        float elapsedTime = 0f;
+        //Vector3 targetScale = _originalScale * scaleFactor;
+
+        // Escalar hacia arriba
+        while (elapsedTime < _duration)
+        {
+            elapsedTime += Time.deltaTime;
+            _txtSecondsInGame.transform.localScale = Vector3.Lerp(_originalScale, _newScale, elapsedTime / _duration);
+
+            yield return null;
+        }
+
+        //yield return new WaitForSeconds(0.25f); // Pequeña pausa antes de volver a la escala original
+
+        elapsedTime = 0f;
+
+        // Regresar a la escala original
+        while (elapsedTime < _duration)
+        {
+            elapsedTime += Time.deltaTime;
+            _txtSecondsInGame.transform.localScale = Vector3.Lerp(_newScale, _originalScale, elapsedTime / _duration);
+
+            yield return null;
+        }
     }
 }
