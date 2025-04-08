f099b1ddff22dc575e7aaec5b8e2cb19891d65a5
Matias Sepulveda <ezesepulveda444@gmail.com>
2025-04-08 17:37:48 -0300

    El ascensor anda mejor!!

---
+++ Assets/03_Scripts/07_Objects/Platforms/Elevator/ReturnElevator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnElevator : MonoBehaviour, IInteracteable
+{
+    [SerializeField] private Elevator _elevator;
+
+    public void Active()
+    {
+        if (_elevator.isNotMove)
+        {
+            _elevator.isNotMove = false;
+
+            //_elevator.IsActiveElevator = true;
+
+            _elevator.ActiveElevator(true);
+        }
+    }
+
+    public void Deactive()
+    {
+        
+    }
+}
