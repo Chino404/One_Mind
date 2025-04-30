8c01b385d7ffb8861f43435f531c95f93227e15c
matii03 <matiasmonetti19@gmail.com>
2025-04-30 18:48:44 -0300

    configuracion de video (resolucion y modo pantalla completa)

---
+++ Assets/03_Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private Resolution[] _resolusions;
+
+    public TMP_Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        _resolusions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < _resolusions.Length; i++)
+        {
+            string option = _resolusions[i].width + "x" + _resolusions[i].height;
+            options.Add(option);
+
+            if (_resolusions[i].width == Screen.currentResolution.width && _resolusions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+
+
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        SetResolution(currentResolutionIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolusions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
