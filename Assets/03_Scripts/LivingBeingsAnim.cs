7792fc6225f8109fdd95ab7b9b3467d4945af9ea
matii03 <matiasmonetti19@gmail.com>
2025-05-02 19:30:40 -0300

    respiracion de los personajes y empece con coleccionable lvl 4

--- Assets/03_Scripts/LivingBeingsAnim.cs
+++ Assets/03_Scripts/LivingBeingsAnim.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class SkeletonAnim : MonoBehaviour
+public class LivingBeingsAnim : MonoBehaviour
 {
 
     private Animation _animation;
