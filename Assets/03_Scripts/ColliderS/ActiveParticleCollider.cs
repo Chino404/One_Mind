d3fbc7bb77fe63b51fc1eedccf5bcb0815a6fa83
matii03 <matiasmonetti19@gmail.com>
2025-05-09 20:05:38 -0300

    animacion y particulas del muñeco

--- Assets/03_Scripts/ColliderS/ActiveParticleCollider.cs
+++ Assets/03_Scripts/ColliderS/ActiveParticleCollider.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class ActiveParticleCollider : MonoBehaviour
 {
     public ParticleSystem particles;
+    public int layer;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Characters>())
+       
+        if (other.GetComponent<Characters>() || other.gameObject.layer == layer)
+        {
+            Debug.Log("particulas");
             particles.Play();
+        }
     }
 }
---
+++ Assets/03_Scripts/DummieAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummieAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            int random = Random.Range(0, 2);
+            Debug.Log(random);
+            if (random == 0) _animator.SetTrigger("Hit1");
+            if (random == 1) _animator.SetTrigger("Hit2");
+        }
+    }
+}
